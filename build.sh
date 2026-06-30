2d62b2ac403db113e56def8e37287855c4c25c2d
devndataai <mjpythoncode@gmail.com>
2026-06-30 12:24:23 +0530

    2
    2

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+pip install gunicorn whitenoise
 rm -rf staticfiles
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
